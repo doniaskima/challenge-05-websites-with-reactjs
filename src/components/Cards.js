c9fbe1c604157f3a3f09958adf3dad90cf9d9b71
doniaskima <doniaskima344@gmail.com>
2022-07-31 23:11:25 +0100

    struggle is true

---
+++ src/components/Cards.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+   <></>
+  )
+}
+
+export default Cards
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import FirstSection from "../FirstSection";
+import Footer from "../Footer";
+
+function Home() {
+    return (
+        <>
+            <FirstSection />
+            <Cards />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
