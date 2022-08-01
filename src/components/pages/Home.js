9a05b3f4de9bcf299c95d18f77ec681e3544f898
doniaskima <doniaskima344@gmail.com>
2022-08-01 12:40:15 +0100

    fixing cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,15 +3,18 @@ import "../../App.css";
 import Cards from "../Cards";
 import FirstSection from "../FirstSection";
 import Footer from "../Footer";
+import styled from "styled-components";
 
 function Home() {
-    return (
-        <>
-            <FirstSection />
-            <Cards />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <FirstSection />
+
+      <Cards />
+
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
