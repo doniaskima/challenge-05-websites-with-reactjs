960e608b9a3e1025b9f4c08f4888afd1f82aaf4a
doniaskima <doniaskima344@gmail.com>
2022-08-02 01:47:13 +0100

    end

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -1,9 +1,138 @@
 import React from 'react'
-
+import styled from "styled-components";
+import img1 from "../images/img-8.jpg";
+import img2 from "../images/img-1.jpg";
+import img3 from "../images/paris.jpg";
+import img4 from "../images/img-3.jpg";
+import img5 from "../images/img-4.jpg";
+import img6 from "../images/img-7.jpg";
+import info1 from "../images/info1.png";
+import info2 from "../images/info2.png";
+import info3 from "../images/info3.png";
 const Recommend = () => {
+    const data=[
+        {
+      image: img6,
+      title: "Singapore",
+      subTitle: "Singapore, officialy thr Republic of Singapore, is a",
+      cost: "38,800",
+      duration: "Approx 2 night trip",
+    },
+    {
+      image: img2,
+      title: "Thailand",
+      subTitle: "Thailand is a Southeast Asia country. It's known for",
+      cost: "54,200",
+      duration: "Approx 2 night trip",
+    },
+    {
+      image: img3,
+      title: "Paris",
+      subTitle: "Paris, France's capital, is a major European city and a",
+      cost: "45,500",
+      duration: "Approx 2 night trip",
+    },
+    {
+      image: img4,
+      title: "New Zealand",
+      subTitle: "New Zealand is an island country in the",
+      cost: "24,100",
+      duration: "Approx 1 night trip",
+    },
+    {
+      image: img5,
+      title: "Bora Bora",
+      subTitle: "Bora Bora is a small South Pacific island northwest of",
+      cost: "95,400",
+      duration: "Approx 2 night 2 day trip",
+    },
+    {
+      image: img1,
+      title: "London",
+      subTitle: "London, the capital of England and the United",
+      cost: "38,800",
+      duration: "Approx 3 night 2 day trip",
+    },
+     ]
+
+   
   return (
-  <></>
+      <Section id="recommend">
+          <div className="destinations">
+              {data.map((destination) => {
+                  return (
+                    <div className="destination">
+                      <img src={destination.image} alt="" />
+                      <h3>{destination.title}</h3>
+                      <p>{destination.subTitle}</p>
+                      <div className="info">
+                        <div className="services">
+                          <img src={info1} alt="" />
+                          <img src={info2} alt="" />
+                          <img src={info3} alt="" />
+                        </div>
+                      </div>
+                    </div>
+                  );
+              })}
+          </div>
+ </Section>
   )
 }
 
-export default Recommend
+export default Recommend;
+
+const Section = styled.section`
+  padding: 2rem 0;
+  .title {
+    text-align: center;
+  }
+  .destinations {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+    padding: 0 3rem;
+    .destination {
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      background-color: #8338ec14;
+      border-radius: 1rem;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        transform: translateX(0.4rem) translateY(-1rem);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      }
+      img {
+        width: 100%;
+      }
+      .info {
+        display: flex;
+        align-items: center;
+        .services {
+          display: flex;
+          gap: 0.3rem;
+          img {
+            border-radius: 1rem;
+            background-color: #4d2ddb84;
+            width: 2rem;
+            padding: 0.3rem 0.4rem;
+          }
+        }
+        display: flex;
+        justify-content: space-between;
+      }
+      .distance {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 768px) {
+    .destinations {
+      grid-template-columns: 1fr;
+      padding: 0;
+    }
+  }
+`;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import avatar1 from "../images/avatarimg1.jpg";
+import avatar2 from "../images/avatarimg2.jpg";
+import avatar3 from "../images/avatarimg3.jpg";
 const Testimonials = () => {
   return (
-    <></>
-  )
-}
+    <Section id="testimonials">
+      <div className="title">
+        <h2>happy cutomers</h2>
+      </div>
+      <div className="testimonials">
+        <div className="testimonial">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            asperiores eaque.
+          </p>
+          <div className="info">
+            <img src={avatar1} alt="" />
+            <div className="setails">
+              <h4>Camilia</h4>
+              <span>web devolver</span>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            asperiores eaque.
+          </p>
+          <div className="info">
+            <img src={avatar2} alt="" />
+            <div className="setails">
+              <h4>Camilia</h4>
+              <span>web devolver</span>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            asperiores eaque.
+          </p>
+          <div className="info">
+            <img src={avatar3} alt="" />
+            <div className="setails">
+              <h4>Camilia</h4>
+              <span>web devolver</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
+const Section = styled.section`
+   .title {
+     margin: 5rem 0;
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  .testimonials {
+    display: flex;
+    justify-content: center;
+    margin: 0 2rem;
+    gap: 2rem;
+    .testimonial {
+      background-color: aliceblue;
+      padding: 2rem;
+      border-radius: 0.5rem;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        transform: translateX(0.4rem) translateY(-1rem);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      }
+      .info {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        align-items: center;
+        margin-top: 1rem;
+        img {
+          border-radius: 3rem;
+          height: 3rem;
+        }
+        .details {
+          span {
+            font-size: 0.9rem;
+          }
+        }
+      }
+    }
+  }
+`;
