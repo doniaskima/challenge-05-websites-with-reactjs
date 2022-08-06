2d992e6ee218c9c4d7533ab79b80f95e909c15ab
doniaskima <doniaskima344@gmail.com>
2022-08-06 18:18:43 +0100

    notification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import {uiActions} from "./store/ui-slice"
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-
+import Notification from "./components/Notification";
 function App() {
+  let isFirstRender = true;
+  const cart = useSelector(state => state.cart)
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const sendRequest = async () => {
+      dispatch(uiActions.showNotifications({
+        open: true,
+        message: "Sending Request",
+        type: "Warning"
+      }))
+      const res = await fetch('https://redux-70d1f-default-rtdb.firebaseio.com/cartItems.json',
+        {
+          method: "PUT",
+          body: JSON.stringify(cart)
+        }
+      );
+      const data = await res.json();
+      //send state as Request is successful 
+      dispatch(uiActions.showNotifications({
+        open: true,
+        message: "Sent Request To Databases Succeffulyy",
+        type: "success"
+      }))
+    };
+    sendRequest().catch(err => {
+      dispatch(uiActions.showNotifications({
+        open: true,
+        message: "Sending Request Failed",
+        type: "error"
+      }))
+    })
+  }, [cart]);
   return (
     <div className="App">
+      
+   
+    
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
-      
+
     </div>
   );
 }
---
+++ src/store/ui-slice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const uiSlice = createSlice({
+    name: 'ui',
+    initialState: { notification: null },
+    reducers: {
+        showNotifications(state, action) {
+            state.notification = {
+                message: action.payload.message,
+                type: action.payload.type,
+                open: action.payload.open
+            }
+        }
+    }
+})
+
+export const uiActions = uiSlice.actions;
+export default uiSlice;
